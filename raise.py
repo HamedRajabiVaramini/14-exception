ac96b458bc758111f360d8584db582313b0510e7
Hamed Rajabi Varamini <hamed.rajabi@targetofs.com>
2024-04-17 10:36:20 +0400

    Chore;Adds one example for raising custom exception

---
+++ raise.py
@@ -0,0 +1,11 @@
+ # type: ignore
+'''
+simple_programming_
+
+This code uses raise to throw an exception
+under a certain condition
+''' 
+x = -6
+if x < 0 || x > 10:
+    raise Exception("'x' must be in the range 0 < x < 10")
+
